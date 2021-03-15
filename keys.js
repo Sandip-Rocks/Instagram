6f9ca43d3ec24d6f3ab3340e7b836ee2b9158716
Sandip_Rocks <sandip4991@gmail.com>
2021-03-15 12:31:58 +0530

    user DB created

---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:'mongodb+srv://admin:admin@123@cluster0.kjoai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,42 @@
 const express=require('express');
 const router=express.Router();
+const mongoose=require('mongoose');
+const User=mongoose.model('User');
+
 
 router.get('/',(req,res,next)=>{
     res.send('Hello Auth')
 })
 router.post('/signup',(req,res,next)=>{
-    res.send(req.body)
+    // res.send(req.body)
+    const {name,email,password}=req.body;
+    if(!email||!password||!name){
+        return res.status(422).json({
+            error:"Please add all the fields"
+        })
+    }
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"User already exist with that email"})
+        }
+        const user=new User({
+            email,
+            password,
+            name
+        })
+        user.save()
+        .then(user=>{
+            res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+    
+    
 })
 module.exports=router;
